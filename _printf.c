30ba6e1739f8a6a04abb57823b3be2b0194d45dc
denis <denisstillwaters@gmail.com>
2023-04-25 20:11:06 -0700

    retyped betty style comment

--- _printf.c
+++ _printf.c
@@ -4,8 +4,8 @@ void print_buffer(char buffer[], int *buff_ind);
 
 /**
  * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
+ * @format: format
+ * Return: returns chars
  */
 int _printf(const char *format, ...)
 {
@@ -53,9 +53,9 @@ return (printed_chars);
 }
 
 /**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
+ * print_buffer - Prints the contents of buffer
+ * @buffer: this is an Array of chars
+ * @buff_ind: Index of next char length
  */
 void print_buffer(char buffer[], int *buff_ind)
 {
